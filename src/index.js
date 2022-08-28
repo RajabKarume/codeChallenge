559c8145ea06fea375fe36e4330d5e1f5b758647
Rajab <rkarume@gmail.com>
2022-08-28 23:51:24 +0300

    This website can now successfully add comments

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
 // write your code here
+let likeButton = document.getElementById("like-button")
+let likeCount = document.getElementById("like-count").value
+let commentForm = document.getElementById("comment-form")
+
+function getImages(){
+    fetch('http://localhost:3000/images/1') 
+    .then((resp) => resp.json())
+    .then((data) => {
+        document.getElementById("card-title").innerText = data.title;
+        document.getElementById("card-image").src = data.image;
+        document.getElementById("card-image").alt = data.title;
+    });
+}
+getImages();
+
+
+function getComments(){
+    fetch(`http://localhost:3000/images/1/comments/`)
+    .then((resp) => resp.json())
+    .then((data) => {
+        let comments = [];
+        for(let i=0; i<data.length; i++){
+            let arr = data[i].content;
+            comments.push(arr)
+        }
+        console.log(comments)
+        document.getElementById("comments-list").innerHTML = comments.map((comments) =>`<li>${comments}</li>`).join("")
+    })
+}
+getComments(); 
+
+
+commentForm.addEventListener("submit", (e) => {e.preventDefault()
+    let newComment = document.getElementById("comment").value;
+    document.getElementById("comment-form").innerHTML += `<li>${newComment}</li>`;
+    });
+
+// likeCount.addEventListener("click", () => )
+console.log(likeCount)
+
+
